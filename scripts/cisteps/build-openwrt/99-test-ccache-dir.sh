8de4c1419803c3695e6589b8a63c4ac3bf40fe57
berlin4apk <83521068+berlin4apk@users.noreply.github.com>
2023-04-04 07:54:30 +0200

    Create 99-test-ccache-dir.sh

---
+++ scripts/cisteps/build-openwrt/99-test-ccache-dir.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# set -eo pipefail
+
+echo "start ON HOST ###########################################"
+        realfs=$(df -x devtmpfs | tail -n +2 | awk '{print $6;}' | xargs)
+        find ${realfs} -iname "*ccache" -ls 2>&1 | grep -v "Permission denied"
+echo "end ON HOST ###########################################"
+
+
+# shellcheck disable=SC1090
+source "${HOST_WORK_DIR}/scripts/host/docker.sh"
+
+echo "started=1" >> $GITHUB_OUTPUT
+docker_exec -e MODE=m "${BUILDER_CONTAINER_ID}" "${BUILDER_WORK_DIR}/scripts/test-ccache-dir.sh"
+echo "status=success" >> $GITHUB_OUTPUT
