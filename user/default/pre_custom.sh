fe85497dee0fc6ec7b17bcf6db163a2f2be98783
berlin4apk <83521068+berlin4apk@users.noreply.github.com>
2023-04-14 09:17:46 +0200

    Update pre_custom.sh

--- user/default/pre_custom.sh
+++ user/default/pre_custom.sh
@@ -33,10 +33,37 @@ _install_if_not_has_command() {
     fi
 }
 
+_install_apt_deb() {
+export DEBIAN_FRONTEND=noninteractive
+#    sudo -E apt-get -qq install --no-upgrade "$1" || $(sudo -E apt-get -qq update && sudo -E apt-get -qq install --no-upgrade "$1")
+    sudo -E apt-get --yes install --no-upgrade "$1" ||:
+    if "x0" = "x$?"; then
+         sudo -E apt-get -qq --yes update
+         sudo -E apt-get --yes install --no-upgrade "$1"
+    fi
+}
 
 _install_if_not_has_command genisoimage
 _install_if_not_has_command tree
 #_install_if_not_has_command foofeeoo
+#
+# _install_if_not_has_command redis-cli
+# _install_if_not_has_command python3-redis
+# _install_if_not_has_command python3-progress
+# _install_if_not_has_command python3-progressbar
+# _install_if_not_has_command python3-humanize
+
+_install_apt_deb redis-cli python3-redis python3-progress python3-progressbar python3-humanize
+
+wget -O ccache-download-redis https://github.com/ccache/ccache/raw/v4.8/misc/download-redis
+wget -O ccache-upload-redis https://github.com/ccache/ccache/raw/v4.8/misc/upload-redis
+
+echo "eda111306e3d65ac61a86811596d18c283711f00bb5cd76d23df1f4b885d812a *ccache-download-redis" | sha256sum -c -
+echo "5305b25e1534601fd102f295a40f5b670dfd63b5f525cd6079d0c5ac86a46c3c *ccache-upload-redis" | sha256sum -c -
+
+sudo chmod 755 ccache-upload-redis ccache-download-redis
+sudo cp -p ccache-upload-redis ccache-download-redis /usr/local/bin/
+
 
 echo "= UIDs in docker ==== $0 ==============================================="
 	set -vx
