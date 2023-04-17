099d0c70fc457c758a4a495d832652d2553e5316
berlin4apk <83521068+berlin4apk@users.noreply.github.com>
2023-04-17 11:04:37 +0200

    add user/22.03.4-rsync

---
+++ user/22.03.4-rsync/custom_01.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "= $0 ==== start ==========================================="
+# rm ./package/feeds/packages/node
+# rm ./package/feeds/packages/node-*
+# ./scripts/feeds update node
+# ./scripts/feeds install -a -p node
+# make defconfig
+
+set -x
+#mkdir -p dl
+#mkdir -p /home/builder/dl
+#smkdir -p /home/builder/openwrt/dl
+
+tree -a ./
+echo "= $0 ==== end ==========================================="
