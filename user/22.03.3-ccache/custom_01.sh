7a3f432e9e544aea158fd3951e16dddfcfa7162d
berlin4apk <83521068+berlin4apk@users.noreply.github.com>
2023-04-17 14:28:22 +0200

    add user/22.03.3-ccache

---
+++ user/22.03.3-ccache/custom_01.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "= $0 ==== start ==========================================="
+# rm ./package/feeds/packages/node
+# rm ./package/feeds/packages/node-*
+# ./scripts/feeds update node
+# ./scripts/feeds install -a -p node
+# make defconfig
+
+set -x
+# mkdir -p dl ## FIXME ##
+# mkdir -p /home/builder/dl  ## FIXME ##
+# mkdir -p /home/builder/openwrt/dl  ## FIXME ##
+
+tree -a ./
+echo "= $0 ==== end ==========================================="
