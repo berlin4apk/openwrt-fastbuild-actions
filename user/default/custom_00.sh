a6f0e449cf6485391bf1bdbd7a07be4857d9de38
berlin4apk <83521068+berlin4apk@users.noreply.github.com>
2023-04-18 01:08:50 +0200

    update user/default/custom_00.sh

--- user/default/custom_00.sh
+++ user/default/custom_00.sh
@@ -10,12 +10,17 @@ echo "CONFIG_CCACHE=y" | tee -a .config
 # CONFIG_CCACHE_DIR="/home/builder/.ccache"
 #CONFIG_CCACHE_DIR="/dev/shm/$(id -u)/ccache/"
 # CONFIG_CCACHE_DIR="/dev/shm/1001/ccache/"
+
+echo "BUILDER_CCACHE_DIR ${BUILDER_CCACHE_DIR}"
 echo "CONFIG_CCACHE_DIR=${BUILDER_CCACHE_DIR}/" | tee -a .config
 
 # CONFIG_DOWNLOAD_FOLDER="/tmp/DLccache"
 #CONFIG_DOWNLOAD_FOLDER="/dev/shm/$(id -u)/DLccache"
 # CONFIG_DOWNLOAD_FOLDER="/dev/shm/1001/DLccache"
 # CONFIG_DOWNLOAD_FOLDER="${BUILDER_DLCCACHE_DIR}/"
+
+echo "BUILDER_DLCCACHE_DIR ${BUILDER_DLCCACHE_DIR}"
+
 echo "CONFIG_DOWNLOAD_FOLDER=${BUILDER_DLCCACHE_DIR}/" | tee -a .config
 
 echo "# DL_DIR=${BUILDER_DLCCACHE_DIR}/" | tee -a .config
